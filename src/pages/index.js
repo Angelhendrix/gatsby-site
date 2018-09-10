fa136cc0eb7fe15016b3dda1e61f8f12bcf05f23
Angel Hernandez <ale12jr415@gmail.com>
2018-09-09 17:35:37 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>My name is Angel, and i'm super excited to get started.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
